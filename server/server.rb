ad8652029b7259b4f7a8ed8234b99930d26e4d6d
Seth Fitzsimmons <seth@simplegeo.com>
2010-11-15 10:12:53 -0700

    Return some actual GeoJSON

--- server/server.rb
+++ server/server.rb
@@ -3,6 +3,16 @@ require 'sinatra'
 
 get '/0.1/features/:id.json' do
   {
-    :foo => "bar",
+    "type" => "Feature",
+    "geometry" => {
+      "type" => "Point",
+      "coordinates" => [
+        -122.938,
+        37.079
+      ]
+    },
+    "properties" => {
+        "type" => "place"
+    }
   }.to_json
 end

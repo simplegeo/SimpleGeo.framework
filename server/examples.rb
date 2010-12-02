c6cb55da03ec009de7864ef38428ebffb31cbe3c
Seth Fitzsimmons <seth@simplegeo.com>
2010-12-02 13:51:54 -0600

    Better sample context response

--- server/examples.rb
+++ server/examples.rb
@@ -1,32 +1,32 @@
 BURGER_MASTER = <<-EOS
 {
     "geometry": {
-        "type": "Point", 
+        "type": "Point",
         "coordinates": [
-            -122.937467, 
+            -122.937467,
             47.046962
         ]
-    }, 
-    "type": "Feature", 
-    "id": "SG_4CsrE4oNy1gl8hCLdwu0F0_47.046962_-122.937467@1290636830", 
+    },
+    "type": "Feature",
+    "id": "SG_4CsrE4oNy1gl8hCLdwu0F0_47.046962_-122.937467@1290636830",
     "properties": {
-        "city": "Olympia", 
-        "name": "Burger Master West Olympia", 
+        "city": "Olympia",
+        "name": "Burger Master West Olympia",
         "tags": [
             "eating"
-        ], 
-        "country": "us", 
-        "phone": "3603575451", 
-        "state": "WA", 
-        "address": "2820 Harrison Ave NW", 
-        "owner": "simplegeo", 
+        ],
+        "country": "us",
+        "phone": "3603575451",
+        "state": "WA",
+        "address": "2820 Harrison Ave NW",
+        "owner": "simplegeo",
         "categories": [
             [
-                "Food & Drink", 
-                "Restaurants", 
+                "Food & Drink",
+                "Restaurants",
                 ""
             ]
-        ], 
+        ],
         "postcode": "98502"
     }
 }
@@ -34,228 +34,408 @@ EOS
 
 BURGERS = <<-EOS
 {
-    "total": 7, 
-    "type": "FeatureCollection", 
+    "total": 7,
+    "type": "FeatureCollection",
     "features": [
         {
             "geometry": {
-                "type": "Point", 
+                "type": "Point",
                 "coordinates": [
-                    -122.937467, 
+                    -122.937467,
                     47.046962
                 ]
-            }, 
-            "type": "Feature", 
-            "id": "SG_4CsrE4oNy1gl8hCLdwu0F0_47.046962_-122.937467@1290636830", 
+            },
+            "type": "Feature",
+            "id": "SG_4CsrE4oNy1gl8hCLdwu0F0_47.046962_-122.937467@1290636830",
             "properties": {
-                "city": "Olympia", 
-                "name": "Burger Master West Olympia", 
+                "city": "Olympia",
+                "name": "Burger Master West Olympia",
                 "tags": [
                     "eating"
-                ], 
-                "country": "us", 
-                "phone": "3603575451", 
-                "state": "WA", 
-                "address": "2820 Harrison Ave NW", 
-                "owner": "simplegeo", 
+                ],
+                "country": "us",
+                "phone": "3603575451",
+                "state": "WA",
+                "address": "2820 Harrison Ave NW",
+                "owner": "simplegeo",
                 "categories": [
                     [
-                        "Food & Drink", 
-                        "Restaurants", 
+                        "Food & Drink",
+                        "Restaurants",
                         ""
                     ]
-                ], 
+                ],
                 "postcode": "98502"
             }
-        }, 
+        },
         {
             "geometry": {
-                "type": "Point", 
+                "type": "Point",
                 "coordinates": [
-                    -122.941154, 
+                    -122.941154,
                     47.051511
                 ]
-            }, 
-            "type": "Feature", 
-            "id": "SG_6yi4PHOtjbPShBKPdXDiGU_47.051511_-122.941154@1290636830", 
+            },
+            "type": "Feature",
+            "id": "SG_6yi4PHOtjbPShBKPdXDiGU_47.051511_-122.941154@1290636830",
             "properties": {
-                "website": "www.redrobin.com", 
-                "city": "Olympia", 
-                "name": "Red Robin Gourmet Burgers", 
+                "website": "www.redrobin.com",
+                "city": "Olympia",
+                "name": "Red Robin Gourmet Burgers",
                 "tags": [
                     "restaurants"
-                ], 
-                "country": "us", 
-                "phone": "3607549620", 
-                "state": "WA", 
-                "address": "600 Cooper Pt Rd NW", 
-                "owner": "simplegeo", 
+                ],
+                "country": "us",
+                "phone": "3607549620",
+                "state": "WA",
+                "address": "600 Cooper Pt Rd NW",
+                "owner": "simplegeo",
                 "categories": [
                     [
-                        "Food & Drink", 
-                        "Restaurants", 
+                        "Food & Drink",
+                        "Restaurants",
                         ""
                     ]
-                ], 
+                ],
                 "postcode": "98502"
             }
-        }, 
+        },
         {
             "geometry": {
-                "type": "Point", 
+                "type": "Point",
                 "coordinates": [
-                    -122.908612, 
+                    -122.908612,
                     46.999576
                 ]
-            }, 
-            "type": "Feature", 
-            "id": "SG_0QVLLlZ3qvPUM7Vq0bc0UN_46.999576_-122.908612@1290636830", 
+            },
+            "type": "Feature",
+            "id": "SG_0QVLLlZ3qvPUM7Vq0bc0UN_46.999576_-122.908612@1290636830",
             "properties": {
-                "city": "Olympia", 
-                "name": "Burger King", 
+                "city": "Olympia",
+                "name": "Burger King",
                 "tags": [
                     "eating"
-                ], 
-                "country": "us", 
-                "phone": "3605071862", 
-                "state": "WA", 
-                "address": "5252 Capitol Blvd S", 
-                "owner": "simplegeo", 
+                ],
+                "country": "us",
+                "phone": "3605071862",
+                "state": "WA",
+                "address": "5252 Capitol Blvd S",
+                "owner": "simplegeo",
                 "categories": [
                     [
-                        "Food & Drink", 
-                        "Restaurants", 
+                        "Food & Drink",
+                        "Restaurants",
                         ""
                     ]
-                ], 
+                ],
                 "postcode": "98501"
             }
-        }, 
+        },
         {
             "geometry": {
-                "type": "Point", 
+                "type": "Point",
                 "coordinates": [
-                    -122.839138, 
+                    -122.839138,
                     47.047588
                 ]
-            }, 
-            "type": "Feature", 
-            "id": "SG_2uWiJ7cqCQkktN5qWkqU8N_47.047588_-122.839138@1290636830", 
+            },
+            "type": "Feature",
+            "id": "SG_2uWiJ7cqCQkktN5qWkqU8N_47.047588_-122.839138@1290636830",
             "properties": {
-                "city": "Olympia", 
-                "name": "Paul's Burger & Teriyaki", 
+                "city": "Olympia",
+                "name": "Paul's Burger & Teriyaki",
                 "tags": [
-                    "eating", 
+                    "eating",
                     "restaurants"
-                ], 
-                "country": "us", 
-                "phone": "3604553328", 
-                "state": "WA", 
-                "address": "3959 Martin Way E", 
-                "owner": "simplegeo", 
+                ],
+                "country": "us",
+                "phone": "3604553328",
+                "state": "WA",
+                "address": "3959 Martin Way E",
+                "owner": "simplegeo",
                 "categories": [
                     [
-                        "Food & Drink", 
-                        "Restaurants", 
+                        "Food & Drink",
+                        "Restaurants",
                         ""
                     ]
-                ], 
+                ],
                 "postcode": "98506"
             }
-        }, 
+        },
         {
             "geometry": {
-                "type": "Point", 
+                "type": "Point",
                 "coordinates": [
-                    -122.823169, 
+                    -122.823169,
                     46.996238
                 ]
-            }, 
-            "type": "Feature", 
-            "id": "SG_5fqftwOMv8w2nKC7nzY6UD_46.996238_-122.823169@1290636830", 
+            },
+            "type": "Feature",
+            "id": "SG_5fqftwOMv8w2nKC7nzY6UD_46.996238_-122.823169@1290636830",
             "properties": {
-                "city": "Lacey", 
-                "name": "Burger King", 
+                "city": "Lacey",
+                "name": "Burger King",
                 "tags": [
                     "eating"
-                ], 
-                "country": "us", 
-                "phone": "3604931990", 
-                "state": "WA", 
-                "address": "4830 Yelm Hwy SE", 
-                "owner": "simplegeo", 
+                ],
+                "country": "us",
+                "phone": "3604931990",
+                "state": "WA",
+                "address": "4830 Yelm Hwy SE",
+                "owner": "simplegeo",
                 "categories": [
                     [
-                        "Food & Drink", 
-                        "Restaurants", 
+                        "Food & Drink",
+                        "Restaurants",
                         ""
                     ]
-                ], 
+                ],
                 "postcode": "98503"
             }
-        }, 
+        },
         {
             "geometry": {
-                "type": "Point", 
+                "type": "Point",
                 "coordinates": [
-                    -122.786868, 
+                    -122.786868,
                     47.052215
                 ]
-            }, 
-            "type": "Feature", 
-            "id": "SG_1f1u00iP3nmK13v7iafwx9_47.052215_-122.786868@1290636830", 
+            },
+            "type": "Feature",
+            "id": "SG_1f1u00iP3nmK13v7iafwx9_47.052215_-122.786868@1290636830",
             "properties": {
-                "city": "Olympia", 
-                "name": "Norma's Burger Hut", 
+                "city": "Olympia",
+                "name": "Norma's Burger Hut",
                 "tags": [
                     "restaurants"
-                ], 
-                "country": "us", 
-                "phone": "3604566547", 
-                "state": "WA", 
-                "address": "7210 Martin Way E", 
-                "owner": "simplegeo", 
+                ],
+                "country": "us",
+                "phone": "3604566547",
+                "state": "WA",
+                "address": "7210 Martin Way E",
+                "owner": "simplegeo",
                 "categories": [
                     [
-                        "Food & Drink", 
-                        "Restaurants", 
+                        "Food & Drink",
+                        "Restaurants",
                         ""
                     ]
-                ], 
+                ],
                 "postcode": "98516"
             }
-        }, 
+        },
         {
             "geometry": {
-                "type": "Point", 
+                "type": "Point",
                 "coordinates": [
-                    -122.765139, 
+                    -122.765139,
                     47.062385
                 ]
-            }, 
-            "type": "Feature", 
-            "id": "SG_4P24zSCw5icnDxUeXRdMTh_47.062385_-122.765139@1290636830", 
+            },
+            "type": "Feature",
+            "id": "SG_4P24zSCw5icnDxUeXRdMTh_47.062385_-122.765139@1290636830",
             "properties": {
-                "city": "Olympia", 
-                "name": "Burger King", 
+                "city": "Olympia",
+                "name": "Burger King",
                 "tags": [
                     "eating"
-                ], 
-                "country": "us", 
-                "phone": "3604566552", 
-                "state": "WA", 
-                "address": "1609 Marvin Rd NE", 
-                "owner": "simplegeo", 
+                ],
+                "country": "us",
+                "phone": "3604566552",
+                "state": "WA",
+                "address": "1609 Marvin Rd NE",
+                "owner": "simplegeo",
                 "categories": [
                     [
-                        "Food & Drink", 
-                        "Restaurants", 
+                        "Food & Drink",
+                        "Restaurants",
                         ""
                     ]
-                ], 
+                ],
                 "postcode": "98516"
             }
         }
     ]
+}
+EOS
+
+CONTEXT = -<<EOS
+{
+    "demographics": {
+        "metro_score": "9"
+    },
+    "features": [
+        {
+            "abbr": "",
+            "license": "http://creativecommons.org/publicdomain/mark/1.0/",
+            "bounds": [
+                -179.142471,
+                18.930137,
+                179.78115,
+                71.412180
+            ],
+            "category": "National",
+            "id": "SG_3uwSAEdXVBzK1ZER9Nqkdp_45.687160_-112.493107",
+            "name": "United States of America",
+            "subcategory": "",
+            "type": "Boundary"
+        },
+        {
+            "abbr": "CA",
+            "license": "http://creativecommons.org/publicdomain/mark/1.0/",
+            "bounds": [
+                -124.482003,
+                32.528832,
+                -114.131210,
+                42.009517
+            ],
+            "category": "Subnational",
+            "id": "SG_2MySaPILVQG3MoXrsVehyR_37.215297_-119.663837",
+            "name": "California",
+            "subcategory": "State",
+            "type": "Boundary"
+        },
+        {
+            "abbr": "",
+            "license": "http://creativecommons.org/publicdomain/zero/1.0/",
+            "bounds": [
+                -124.733253,
+                32.534621,
+                -114.039345,
+                49.002892
+            ],
+            "category": "Time Zone",
+            "id": "SG_3tLT0I5cOUWIpoVOBeScOx_41.316130_-119.116571",
+            "name": "America/Los_Angeles",
+            "subcategory": "",
+            "type": "Boundary"
+        },
+        {
+            "abbr": "",
+            "license": "http://creativecommons.org/publicdomain/mark/1.0/",
+            "bounds": [
+                -123.173824,
+                37.639830,
+                -122.28178,
+                37.929824
+            ],
+            "category": "Administrative",
+            "id": "SG_7TAYWdlPlAIzUDT7MVwxmZ_37.759717_-122.693971",
+            "name": "San Francisco",
+            "subcategory": "County",
+            "type": "Boundary"
+        },
+        {
+            "abbr": "",
+            "license": "http://creativecommons.org/publicdomain/mark/1.0/",
+            "bounds": [
+                -123.173824,
+                37.639830,
+                -122.28178,
+                37.929824
+            ],
+            "category": "Municipal",
+            "id": "SG_1mNfKHr5aXH7LWgmZL8Uq7_37.759717_-122.693971",
+            "name": "San Francisco",
+            "subcategory": "City",
+            "type": "Boundary"
+        },
+        {
+            "abbr": "",
+            "license": "http://creativecommons.org/publicdomain/mark/1.0/",
+            "bounds": [
+                -123.173824,
+                37.639830,
+                -122.28178,
+                37.929824
+            ],
+            "category": "School District",
+            "id": "SG_4wyrIh6TQId1MiL2cfYa5d_37.759717_-122.693971",
+            "name": "San Francisco Unified School District",
+            "subcategory": "Unified",
+            "type": "Boundary"
+        },
+        {
+            "abbr": "",
+            "license": "http://creativecommons.org/publicdomain/mark/1.0/",
+            "bounds": [
+                -122.516667,
+                37.191667,
+                -121.733333,
+                38.041666
+            ],
+            "category": "Urban Area",
+            "id": "SG_4n4ze6xOdAFr0gp1WboZrN_37.551206_-122.127401",
+            "name": "San Francisco1",
+            "subcategory": "",
+            "type": "Boundary"
+        },
+        {
+            "abbr": "",
+            "license": "http://creativecommons.org/publicdomain/mark/1.0/",
+            "bounds": [
+                -122.40499,
+                37.764378,
+                -122.379681,
+                37.783529
+            ],
+            "category": "US Census",
+            "id": "SG_3JxiYHuWo7N9KDYeDSijzl_37.772749_-122.390793",
+            "name": "06075060700",
+            "subcategory": "Tract",
+            "type": "Boundary"
+        },
+        {
+            "abbr": "",
+            "license": "http://creativecommons.org/publicdomain/mark/1.0/",
+            "bounds": [
+                -122.406493,
+                37.749358,
+                -122.379202,
+                37.789791
+            ],
+            "category": "Postal Code",
+            "id": "SG_4iNdS13pIvPoUBWBnq0U2f_37.766945_-122.393570",
+            "name": "94107",
+            "subcategory": "",
+            "type": "Boundary"
+        },
+        {
+            "abbr": "",
+            "license": "http://creativecommons.org/publicdomain/zero/1.0/",
+            "bounds": [
+                -122.398281,
+                37.777028,
+                -122.384281,
+                37.796503
+            ],
+            "category": "Neighborhood",
+            "id": "SG_6Bv7Cw61hmZjfZ8McTGGM2_37.785379_-122.390793",
+            "name": "South Beach",
+            "subcategory": "",
+            "type": "Boundary"
+        },
+        {
+            "abbr": "",
+            "license": "(c) OpenStreetMap (http://openstreetmap.org/) and contributors CC-BY-SA (http://creativecommons.org/licenses/by-sa/2.0/)",
+            "bounds": [
+                -122.39115,
+                37.777233,
+                -122.387775,
+                37.779730
+            ],
+            "category": "Arena",
+            "id": "SG_4H2GqJDZrc0ZAjKGR8qM4D_37.778406_-122.389506",
+            "license": "http://creativecommons.org/licenses/by-sa/2.0/",
+            "name": "AT&T Park",
+            "subcategory": "Stadium",
+            "type": "Entertainment"
+        }
+    ],
+    "weather": {
+        "conditions": "light haze",
+        "temperature": "65F"
+    }
 }
 EOS

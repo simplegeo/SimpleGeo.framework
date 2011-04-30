710c96a6d3ac12920f8a2d11565b2296ed9f2886
Seth Fitzsimmons <seth@mojodna.net>
2011-04-30 13:59:16 -0600

    Remove double retain/autorelease cycle

--- test/SimpleGeoTest.m
+++ test/SimpleGeoTest.m
@@ -47,10 +47,10 @@ NSString * const TEST_URL_PREFIX = @"http://localhost:4567/";
 - (SimpleGeo *)createClient
 {
     NSURL *url = [NSURL URLWithString:TEST_URL_PREFIX];
-    return [[[SimpleGeo clientWithDelegate:self
-                                 consumerKey:@"consumerKey"
-                              consumerSecret:@"consumerSecret"
-                                         URL:url] retain] autorelease];
+    return [SimpleGeo clientWithDelegate:self
+                             consumerKey:@"consumerKey"
+                          consumerSecret:@"consumerSecret"
+                                     URL:url];
 }
 
 - (SGPoint *)point
